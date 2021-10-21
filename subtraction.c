bc8bd478472de7391ebcf07987c98d79cf438638
Himanshu Gupta <59887776+Himanshu-Gupta2001@users.noreply.github.com>
2021-10-21 22:53:11 +0530

    Create subtraction.c

---
+++ subtraction.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,diff;
+	printf("Enter two numbers:");
+	scanf("%d %d",&num1,&num2);
+	diff=num1-num2;
+	printf("\nDifference is : %d",diff);
+	return  0;
+}
